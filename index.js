cd8c86f7c22a56e13f89871faa3e48f3cec0995f
Teejay Dixon <tahjgdixon@gmail.com>
2022-08-12 01:29:50 -0700

    commit tree dc1507d6b062b40c51ebba66f6f8528c721741ee

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+
+  return customerName = 'BOB';
+};
+
+var bestCustomer = undefined;
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+  return bestCustomer;
+};
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+  return bestCustomer;
+};
+
+const leastFavoriteCustomer = undefined;
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'of course, it\'s bob';
+}
+
+
+
+
+
+
+
+
+
